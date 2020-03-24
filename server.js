98609b8dfe7bae6b0cde7429ebe44e4fe2cfad86
AninHuang <lifeanne19904@gmail.com>
2020-03-25 00:11:53 +0800

    Create a web server

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
