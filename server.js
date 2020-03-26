c5cec71c134465f8a14e09ea24b743506796b6af
AninHuang <lifeanne19904@gmail.com>
2020-03-26 21:42:01 +0800

    Set WebSocket Connection

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
+// Express.js - app.listen vs server.listen
+// https://stackoverflow.com/questions/17696801/express-js-app-listen-vs-server-listen
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", socket => {
+  console.log("WebSocket Connection...");
+});
 
 const PORT = process.env.PORT || 8080;
 
