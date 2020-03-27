69aa9de0fa7b570e981942ecff28988913622310
AninHuang <lifeanne19904@gmail.com>
2020-03-27 23:40:39 +0800

    Send msg to a specific chat room

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/message");
 const bot = "Bot";
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require("./utils/user");
 
 // Express.js - app.listen vs server.listen
 // https://stackoverflow.com/questions/17696801/express-js-app-listen-vs-server-listen
@@ -15,15 +21,22 @@ app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", socket => {
   socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    // socket.join(room[, callback])
+    socket.join(user.room);
+
     // 歡迎新進入的使用者
     // 對一個特定的 socket 傳訊息
     socket.emit("message", formatMessage(bot, "Hi This is a realtime app!"));
 
     // 對目前 socket 之外所有線上的 socket 傳訊息
-    socket.broadcast.emit(
-      "message",
-      formatMessage(bot, `${username} has joined the chat room`)
-    );
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(bot, `${username} has joined the chat room`)
+      );
   });
 
   socket.on("chatMsg", msg => {
